fa6867a68f55f9bff01f385b8300fbc18532b946
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-09-15 18:17:00 +0600

    first commit

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueLocalForage from 'vue-localforage'
+import vuetify from './plugins/vuetify'
+
+Vue.use(VueLocalForage)
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
+  VueLocalForage,
   render: h => h(App)
 }).$mount('#app')
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+         <p class="my-4">Welcome to vue-localforage(INDEXEDDB)</p>
+
+      <v-btn-toggle
+        v-model="icon"
+        borderless
+      >
+        <v-btn value="left">
+          <span class="hidden-sm-and-down">Save Data</span>
+
+          <v-icon right>
+            mdi-content-save-outline
+          </v-icon>
+        </v-btn>
+
+        <v-btn value="center">
+          <span class="hidden-sm-and-down">Edit Data</span>
+
+          <v-icon right>
+           mdi-content-save-edit-outline
+          </v-icon>
+        </v-btn>
+
+        <v-btn value="right">
+          <span class="hidden-sm-and-down">Delete Data</span>
+
+          <v-icon right>
+            mdi-delete-outline
+          </v-icon>
+        </v-btn>
+
+        <v-btn value="justify">
+          <span class="hidden-sm-and-down">Show Data</span>
+
+          <v-icon right>
+            mdi-book-open-outline
+          </v-icon>
+        </v-btn>
+      </v-btn-toggle>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '../components/HelloWorld'
+  export default {
+    name: 'Home',
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+    components: {
+      HelloWorld,
+    },
+    data(){
+      return {
+        userData: [
+          {
+            name: "Injam Sonnet",
+            age: 12,
+            address: "Dhaka"
+          },
+          {
+            name: "Faisul Islam",
+            age: 15,
+            address: "Sylhet"
+          }
+        ],
+      }
+    },
+    methods: {
+     saveData(){
+       this.$setItem('userData', this.userData , function(){
+         console.log('Data saved done');
+       })
+     } 
+    },
+    mounted() {
+      this.saveData();
+    }
   }
-}
 </script>
